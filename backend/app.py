0983ea7060950acba9fe6c5ada0c677033193459
Matvey Semenov <matveytri777@gmail.com>
2022-06-19 19:44:57 +0300

    added celery

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,49 @@
+from celery import Celery
+
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_cors import CORS
+from flask_socketio import SocketIO, send
+
+from .logic import OrderTrackManager
+
+manager = OrderTrackManager()
+
+flask_app = Flask(__name__)
+flask_app.host = 'localhost'
+flask_app.config['SECRET_KEY'] = 'SUPERsecret'
+flask_app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+flask_app.config['result_backend'] = 'redis://localhost:6379'
+CORS(flask_app, support_credentials=True)
+
+socket = SocketIO(flask_app, cors_allowed_origins='*')
+
+celery = Celery(flask_app.name, broker=flask_app.config['CELERY_BROKER_URL'])
+celery.conf.update(flask_app.config)
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SUPERsecret'
-socketio = SocketIO(app)
+@celery.task(name='check_sheet_updates')
+def check_sheet_updates():
+    manager.check_delivery_expected()
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Check delivery expected every hour
+    sender.add_periodic_task(60 * 60, check_sheet_updates.s())
 
 
-@app.route("/")
-@socketio.on('connect')
-def test_connect():
-    return "<p>Hello, World!</p>"
+@socket.on('connect')
+def on_connect():
+    jsonable_df = manager.get_jsonable_df_from_db()
+    send(jsonable_df)
+
+@socket.on('message')
+def on_message(message):
+    if message == 'get_data':
+        need_update = manager.check_updates()
+        if need_update:
+            data = manager.get_jsonable_df_from_db()
+            send(data)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+   socket.run(flask_app, host='127.0.0.1', port='5000')
